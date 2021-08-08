fed70b0ae39e5fc349c0195947bdb45f7c8b7bbf
goyeongnam <goyeongnam@localhost>
2021-08-08 23:20:35 +0900

    4단 5단 실

--- src/gugudan/gugudan1.java
+++ src/gugudan/gugudan1.java
@@ -3,28 +3,72 @@ package gugudan;
 public class gugudan1 {
 
 	public static void main(String[] args) {
-		// 2단
-		System.out.println(2*1);
-		System.out.println(2*2);
-		System.out.println(2*3);
-		System.out.println(2*4);
-		System.out.println(2*4);
-		System.out.println(2*5);
-		System.out.println(2*6);
-		System.out.println(2*7);
-		System.out.println(2*8);
-		System.out.println(2*9);
+
 		
-		// 3단
-		System.out.println(3*1);
-		System.out.println(3*2);
-		System.out.println(3*3);
-		System.out.println(3*4);
-		System.out.println(3*5);
-		System.out.println(3*6);
-		System.out.println(3*7);
-		System.out.println(3*8);
-		System.out.println(3*9);
+		//4
+		int result = 4 * 1;
+		System.out.println(result);
+		
+		result = 4*2;
+		System.out.println(result);
+		result = 4*3;
+		System.out.println(result);
+		result = 4*4;
+		System.out.println(result);
+		result = 4*5;
+		System.out.println(result);
+		result = 4*6;
+		System.out.println(result);
+		result = 4*7;
+		System.out.println(result);
+		result = 4*8;
+		System.out.println(result);
+		result = 4*9;
+		System.out.println(result);
+		
+		//5
+		result = 5*1;
+		System.out.println(result);
+		result = 5*2;
+		System.out.println(result);
+		result = 5*3;
+		System.out.println(result);
+		result = 5*4;
+		System.out.println(result);
+		result = 5*5;
+		System.out.println(result);
+		result = 5*6;
+		System.out.println(result);
+		result = 5*7;
+		System.out.println(result);
+		result = 5*8;
+		System.out.println(result);
+		result = 5*9;
+		System.out.println(result);
+		
+		// 2단
+//		System.out.println(2*1);
+//		System.out.println(2*2);
+//		System.out.println(2*3);
+//		System.out.println(2*4);
+//		System.out.println(2*4);
+//		System.out.println(2*5);
+//		System.out.println(2*6);
+//		System.out.println(2*7);
+//		System.out.println(2*8);
+//		System.out.println(2*9);
+//						
+//		
+//		// 3단
+//		System.out.println(3*1);
+//		System.out.println(3*2);
+//		System.out.println(3*3);
+//		System.out.println(3*4);
+//		System.out.println(3*5);
+//		System.out.println(3*6);
+//		System.out.println(3*7);
+//		System.out.println(3*8);
+//		System.out.println(3*9);
 	}
 
 }
