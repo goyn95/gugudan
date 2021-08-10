88bb0adfbfc2f9729be8c327589ad3a6876f2cf9
고영남 <goyn95@naver.com>
2021-08-10 13:47:52 +0900

    구구단 최종 예제 연습_1

---
+++ src/gugudan/R1_Practice.java
@@ -0,0 +1,38 @@
+package gugudan;
+import java.util.Scanner;
+
+public class R1_Practice {
+
+	public static void main(String[] args) {
+		
+		//1번 요구사항
+		//사용자가 입력한 값에따라 크기가 다른 구구단 구
+		
+		/*System.out.println("숫자를 입력하세요. : ");
+		Scanner scan = new Scanner(System.in);
+		int number = scan.nextInt();
+		int result = 0;
+	
+		for (int i = 0; i < number; i++) {
+			result = number * (i + 1);
+			System.out.println(result);
+		}
+	*/
+	
+	//2번 요구사항
+	//사용자가 2개의 값을 입력하여 첫번째 숫자의 구구단을 두번째 숫자의 구구단 만큼 출력
+	
+		System.out.println("숫자 두개를 입력하세요. : ");
+		Scanner scan = new Scanner(System.in);
+		String inputValue = scan.nextLine();
+		String[] splitValue = inputValue.split(",");
+		int firstValue = Integer.parseInt(splitValue[0]);
+		int secondValue = Integer.parseInt(splitValue[1]);
+		
+		for(int i = 0; i < secondValue; i++) {
+			System.out.println(firstValue * (i + 1));
+		}
+
+	}
+
+}
--- src/gugudan/prac.java
+++ src/gugudan/prac.java
@@ -1,26 +1,43 @@
 package gugudan;
+import java.util.Scanner;
 
 public class prac {
 
-	public static int[] cal(int times) {
-		int[] number = new int[9];
-		for (int i = 0; i < number.length; i++) {
-			number[i] = (i + 1) * times;
-		}
-		return number;
-	}
-	
-	public static void print(int[] is) {
-		for (int i = 0; i < 9; i++) {
-			System.out.println(is[i]);
-		}
-	}
-	
 	public static void main(String[] args) {
-		for (int j = 2; j < 10; j++) {
-			print(cal(j));
+		
+		System.out.println("숫자 2개를 입력해 주세요. : ");
+		Scanner scan = new Scanner(System.in);
+		String inputValue = scan.nextLine();
+		inputValue = inputValue.trim();
+		
+		String[] splitValue = inputValue.split(",");
+		int firstValue = Integer.parseInt(splitValue[0]);
+		int secondVale = Integer.parseInt(splitValue[1]);
+		
+		for (int i = 0; i < secondVale; i++) {
+			System.out.println(firstValue * (i + 1));
 		}
-
-	}
+		}
+	
+//	public static int[] cal(int times) {
+//		int[] number = new int[9];
+//		for (int i = 0; i < number.length; i++) {
+//			number[i] = (i + 1) * times;
+//		}
+//		return number;
+//	}
+//	
+//	public static void print(int[] is) {
+//		for (int i = 0; i < 9; i++) {
+//			System.out.println(is[i]);
+//		}
+//	}
+//	
+//	public static void main(String[] args) {
+//		for (int j = 2; j < 10; j++) {
+//			print(cal(j));
+//		}
+//
+//	}
 
 }
